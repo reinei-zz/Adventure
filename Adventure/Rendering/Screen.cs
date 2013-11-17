3e4d3701d5700a87a3f57df12ab2ad5af2d50368
alexanderkunz <alexanderkunz@hotmail.de>
2013-11-17 15:16:48 +0100

    Added button colors and alignment

--- Adventure/Rendering/Screen.cs
+++ Adventure/Rendering/Screen.cs
@@ -26,31 +26,55 @@ namespace Adventure
 
 			protected string Text;
 			protected int X, Y, W, H;
+			protected TCODColor fcol;
+			protected TCODColor bcol;
+			protected TCODAlignment align;
 			protected Action A;
+			protected bool hover;
 
-			public Button(string s, int x, int y, int w, int h, Action a)
+			public Button(string s, int x, int y, int w, int h, TCODColor fcol, TCODColor bcol, TCODAlignment align, Action a)
 			{
 				this.Text = s;
 				this.X = x;
 				this.Y = y;
 				this.W = w;
 				this.H = h;
+				this.fcol = fcol;
+				this.bcol = bcol;
+				this.align = align;
 				this.A = a;
 			}
 
-			public Button(string s, int x, int y, Action a)
-				: this(s, x, y, s.Length, 1, a)
+			public Button(string s, int x, int y, TCODColor fcol, TCODColor bcol, TCODAlignment align, Action a)
+				: this(s, x, y, s.Length, 1, fcol, bcol, align, a)
 			{
 			}
 
 			public void Draw()
 			{
-				GameLoop.Console.print(X, Y, Text);
+				//Color based on mouse hover
+				if (hover)
+				{
+					TCODColor finv = new TCODColor(-((short)fcol.Red - 255), -((short)fcol.Green - 255), -((short)fcol.Blue - 255));
+					TCODColor binv = new TCODColor(-((short)bcol.Red - 255), -((short)bcol.Green - 255), -((short)bcol.Blue - 255));
+					GameLoop.Console.setForegroundColor(finv);
+					GameLoop.Console.setBackgroundColor(binv);
+				}
+				else
+				{
+					GameLoop.Console.setForegroundColor(fcol);
+					GameLoop.Console.setBackgroundColor(bcol);
+				}
+				
+				//Draw
+				GameLoop.Console.printEx(X, Y, TCODBackgroundFlag.Set, align, Text);
 			}
 
-			public void Press(TCODMouseData m)
+			public void Update(TCODMouseData m)
 			{
-				if ((m.CellX >= X && m.CellX <= X + W) && (m.CellY >= Y && m.CellY < Y + H) && m.LeftButton)
+				bool intersects = (m.CellX >= X && m.CellX <= X + W) && (m.CellY >= Y && m.CellY < Y + H);
+				hover = intersects;
+				if (intersects && m.LeftButton)
 				{
 					A();
 				}
